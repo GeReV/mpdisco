d45dc09b6e912c321732b4f1ab29ef691269757b
Amir <amir@fb.com>
2015-03-05 00:24:08 +0200

    Removed a bit more old code.

--- client/js/app.jsx
+++ client/js/app.jsx
@@ -22,53 +22,6 @@ MPDisco.command = MPDisco.network.command.bind(MPDisco.network);
 
 MPDisco.commands = MPDisco.network.commands.bind(MPDisco.network);
 
-MPDisco.Utils = {
-    formatSeconds: function(t) {
-        return Math.floor(+t / 60) + ':' + MPDisco.Utils.pad(+t % 60);
-    },
-    pad: function(n) {
-        if (+n < 10) {
-            return '0' + n;
-        }
-
-        return n;
-    }
-};
-
-//MPDisco.ErrorView = Marionette.ItemView.extend({
-//  className: 'error',
-//
-//  template: 'error',
-//
-//  model: new MPDisco.Error(),
-//
-//  modelEvents: {
-//    change: 'show'
-//  },
-//
-//  socketEvents: {
-//    error: 'show'
-//  },
-//
-//  events: {
-//    'click .close': 'hide'
-//  },
-//
-//  show: function() {
-//    this.render();
-//
-//    this.$el.addClass('shown');
-//  },
-//
-//  hide: function() {
-//    this.$el.removeClass('shown');
-//  }
-//});
-
-//MPDisco.mode = MPDisco.module('MasterMode').Mode;
-
-MPDisco.network.command('status');
-
 window.MPDisco = MPDisco;
 
 React.render(<MPDisco />, document.body);
